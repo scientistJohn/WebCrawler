f52f55ae4afae28c27c173f4ac6adafb4e5868ba
a.lyashenko <a.lyashenko@controlpay.com>
2018-07-27 14:50:09 +0300

    beautifuled

--- src/main/java/org/andrii/utils/App.java
+++ src/main/java/org/andrii/utils/App.java
@@ -1,21 +1,5 @@
 package org.andrii.utils;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("____________________________________________");
